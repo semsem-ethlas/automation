71150e62458906ac8dcf8d77ce368538c96c6ffc
semsem-ethlas <102934232+semsem-ethlas@users.noreply.github.com>
2022-07-21 04:36:55 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# automation
+npm install
+
+1-task for js files
+a- Remove console logs
+b- Apply obfuscation on them
+
+npm run js-cleanup
+
+2-task for game js files
+a- Remove console logs
+b- Apply obfuscation on them
+c- Easy variable search “game over/game start/score” and auto renames them
+d-Check if code contains standard anti dev tool code snippets?
+
+npm run game-cleanup
+
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const stripDebug = require('gulp-strip-debug');
+const minify = require('gulp-minify');
+const JavaScriptObfuscator = require('gulp-javascript-obfuscator');
+const replace = require('gulp-replace');
+const prompt = require('gulp-prompt');
+const print = require('gulp-print').default;
+
+
+gulp.task('js-cleanup', () =>
+  gulp.src('./files/**/*.js')
+    .pipe(prompt.confirm('please continue only if you want to refactor js files (Remove console logs - Apply obfuscation)?'))
+    .pipe(print(filepath => `built: ${filepath}`))
+    .pipe(stripDebug()) 
+    .pipe(JavaScriptObfuscator())
+    .pipe(gulp.dest('./files/'))
+);
+
+gulp.task('game-cleanup', () =>
+  gulp.src('c:/AppServ/www/knife_ninja/**/*.js')
+  .pipe(prompt.confirm('please continue only if you want to refactor games js files (Remove console logs - Apply obfuscation - rename some variable)?'))
+  .pipe(print(filepath => `built: ${filepath}`))
+    .pipe(stripDebug()) 
+    .pipe(JavaScriptObfuscator())
+    .pipe(replace('score', 'h123456'))
+    .pipe(replace('Score', 'h123456'))
+    .pipe(replace('game over', 'h223456'))
+    .pipe(replace('game_over', 'h323456'))
+    .pipe(replace('game start', 'h423456'))
+    .pipe(replace('game_start', 'h523456'))
+    .pipe(replace('Speed', 'h623456'))
+    .pipe(gulp.dest('c:/AppServ/www/knife_ninja/'))
+);
